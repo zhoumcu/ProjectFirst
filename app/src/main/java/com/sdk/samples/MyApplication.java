dea1ce2c4aa8b42e593513b7383063c028f1d954
Administrator <1032324589@qq.com>
2016-08-16 17:45:16 +0800

    test

--- app/src/main/java/com/sdk/samples/MyApplication.java
+++ app/src/main/java/com/sdk/samples/MyApplication.java
@@ -21,5 +21,6 @@ public class MyApplication extends Application{
 		APICloud.initialize(this);//初始化APICloud，SDK中所有的API均需要初始化后方可调用执行
 		//初始化APICloud网络请求框架，如果不需要则忽略，具体使用方式见MainActivity中的Case
 		APICloudHttpClient.createInstance(this);
+		//test
 	}
 }
